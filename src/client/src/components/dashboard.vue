c90e285fc5b42511c06a2e3da16e21fc7adce11e
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-06-07 16:55:56 +0530

    :art: Restrict upvote/downvote for non-authenticated users.

--- src/client/src/components/dashboard.vue
+++ src/client/src/components/dashboard.vue
@@ -2,7 +2,7 @@
 	<div class="ui container">
 		<div id="authButtons" v-if="isLoggedIn()">
 			<div id="comments" class="ui form">
-				<button class="ui button primary inverted right floated" v-on:click="logOut">LogOut</button>
+				<button class="ui button primary inverted right floated" v-on:click="logOut()">LogOut</button>
 				<div class="field">
 					<label>Post comment</label>
 					<textarea id="commentValue"></textarea>
@@ -20,6 +20,34 @@
 				<h1>Comments</h1>
 			</div>
 		</div>
+		<div class="ui small segment" v-for="(comment, index) in comments" v-bind:key="index">
+			<div class="ui grid stackable">
+				<div class="ui six wide column">
+					<span class="ui header">{{ comment.author.name }} ({{ comment.author.username }})</span>
+					<div class="ui hidden divider"></div>
+					<span>{{ comment.value }}</span>
+				</div>
+				<div class="ui four wide column right floated">
+					<div class="ui labeled button" v-bind:class="{ disabled: !isLoggedIn() }" tabindex="0">
+						<div class="ui button" v-on:click="upvoteComment(comment._id)">
+							<i class="thumbs up outline icon"></i> <span>Upvote</span>
+						</div>
+						<a class="ui basic label">
+							{{ comment.upvotes.length }}
+						</a>
+					</div>
+					<div class="ui hidden divider"></div>
+					<div class="ui labeled button" v-bind:class="{ disabled: !isLoggedIn() }" tabindex="0">
+						<div class="ui button" v-on:click="downvoteComment(comment._id)">
+							<i class="thumbs down outline icon"></i> <span>Downvote</span>
+						</div>
+						<a class="ui basic label">
+							{{ comment.downvotes.length }}
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -30,7 +58,13 @@ import { API_BASE } from "../../configs/app";
 export default {
   name: "Dashboard",
   async mounted() {
-    this.showComments();
+    let commentData = await this.populateComments();
+    this.comments = commentData.data;
+  },
+  data() {
+    return {
+      comments: []
+    };
   },
   methods: {
     isLoggedIn() {
@@ -41,48 +75,10 @@ export default {
       }
       return false;
     },
-    async showComments() {
+    async populateComments() {
       const result = await axios.get(`${API_BASE}/comment/all`);
       const { data: comments } = result;
-      if (comments.data.length > 0) {
-        for (let comment of comments.data) {
-          let commentDiv = document.createElement(`div`);
-          let htmlData = `<div class="ui small segment">
-				<div class="ui grid stackable">
-					<div class="ui six wide column">
-						<span class="ui header">${comment.author.name} (${
-            comment.author.username
-          })</span>
-						<div class="ui hidden divider"></div>
-						<span>${comment.value}</span>
-					</div>
-					<div class="ui four wide column right floated">
-						<div class="ui labeled button" tabindex="0">
-							<div class="ui button">
-								<i class="thumbs up outline icon"></i> Upvote
-							</div>
-							<a class="ui basic label">
-								${comment.upvotes.length}
-							</a>
-						</div>
-						<div class="ui hidden divider"></div>
-						<div class="ui labeled button" tabindex="0">
-							<div class="ui button">
-								<i class="thumbs down outline icon"></i> Downvote
-							</div>
-							<a class="ui basic label">
-								${comment.downvotes.length}
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>`;
-          commentDiv.innerHTML = htmlData;
-          document.querySelector("#comments").appendChild(commentDiv);
-        }
-      } else {
-        document.querySelector("#comments").innerHTML = "No comments found.";
-      }
+      return comments;
     },
     async postComment() {
       const config = {
@@ -96,6 +92,20 @@ export default {
         },
         config
       );
+      window.location = "/";
+    },
+    async upvoteComment(commentId) {
+      const config = {
+        headers: { "access-token": localStorage.getItem("accessToken") }
+      };
+      await axios.get(`${API_BASE}/comment/${commentId}/upvote`, config);
+      window.location = "/";
+    },
+    async downvoteComment(commentId) {
+      const config = {
+        headers: { "access-token": localStorage.getItem("accessToken") }
+      };
+      await axios.get(`${API_BASE}/comment/${commentId}/downvote`, config);
       window.location = "/";
     },
     logOut() {
