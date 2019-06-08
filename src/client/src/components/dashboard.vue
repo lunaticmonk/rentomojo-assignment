50c1dab4567f2cfce3620be5bec9b3e0f3b0545c
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-06-08 11:04:33 +0530

    :lipstick: Add some minor UI changes and clean up

--- src/client/src/components/dashboard.vue
+++ src/client/src/components/dashboard.vue
@@ -4,7 +4,7 @@
 			<div id="comments" class="ui form">
 				<button class="ui button primary inverted right floated" v-on:click="logOut()">LogOut</button>
 				<div class="field">
-					<label>Post comment</label>
+					<label><span class="ui huge header">Post comment</span></label>
 					<textarea id="commentValue"></textarea>
 					<div class="ui hidden divider"></div>
 					<button class="ui submit button primary inverted right floated" v-on:click="postComment()">Submit</button>
@@ -17,20 +17,21 @@
 			<button class="ui button primary inverted"><router-link to="/login">Login</router-link></button>
 			<div class="ui hidden divider"></div>
 			<div id="comments">
+				<p>Please create an account and sign in to create/upvote/downvote a comment.</p>
 				<h1>Comments</h1>
 			</div>
 		</div>
 		<div class="ui small segment" v-for="(comment, index) in comments" v-bind:key="index">
 			<div class="ui grid stackable">
 				<div class="ui eight wide column">
-					<span class="ui header">{{ index }} ) </span>
+					<span class="ui header">{{ index + 1 }} ) </span>
 					<span class="ui header">{{ comment.author.name }} ({{ comment.author.username }})</span>
 					<div class="ui hidden divider"></div>
 					<span>{{ comment.value }}</span>
 				</div>
 				<div class="ui four wide column right floated">
 					<div class="ui labeled button" v-bind:class="{ disabled: !isLoggedIn() }" tabindex="0">
-						<div class="ui button" v-on:click="upvoteComment(comment._id, comment.upvotes)">
+						<div class="ui button" v-on:click="upvoteComment(comment._id)">
 							<i class="thumbs up outline icon"></i> <span>Upvote</span>
 						</div>
 						<a class="ui basic label">
@@ -39,7 +40,7 @@
 					</div>
 					<div class="ui hidden divider"></div>
 					<div class="ui labeled button" v-bind:class="{ disabled: !isLoggedIn() }" tabindex="0">
-						<div class="ui button" v-on:click="downvoteComment(comment._id, comment.downvotes)">
+						<div class="ui button" v-on:click="downvoteComment(comment._id)">
 							<i class="thumbs down outline icon"></i> <span>Downvote</span>
 						</div>
 						<a class="ui basic label">
@@ -95,14 +96,14 @@ export default {
       );
       window.location = "/";
     },
-    async upvoteComment(commentId, upvotes) {
+    async upvoteComment(commentId) {
       const config = {
         headers: { "access-token": localStorage.getItem("accessToken") }
       };
       await axios.patch(`${API_BASE}/comment/${commentId}/upvote`, {}, config);
       window.location = "/";
     },
-    async downvoteComment(commentId, downvotes) {
+    async downvoteComment(commentId) {
       const config = {
         headers: { "access-token": localStorage.getItem("accessToken") }
       };
