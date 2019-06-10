bff30daed844d73eac29adac42f928cd8178f47d
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-06-10 17:52:46 +0530

    :package: Add vue-toasted for showing toasts

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,9 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Toasted from "vue-toasted";
 
 import router from "./router";
 
 import "../node_modules/semantic-ui-css/semantic.min.css";
+
+/**
+ * Vue-Toasted for toasts
+ */
+
+let toastOptions = {
+  theme: "toasted-primary",
+  position: "top-right",
+  duration: 1000
+};
+
+Vue.use(Toasted, toastOptions);
 
 Vue.config.productionTip = false;
 
